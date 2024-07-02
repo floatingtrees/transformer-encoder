648b3a56efa31b1c530ca2ea8adbd221ee716b8a
floatingtrees <96200235+floatingtrees@users.noreply.github.com>
2024-07-02 16:54:50 -0700

    did stuff

---
+++ encoder.py
@@ -0,0 +1,24 @@
+import torch
+from x_transformers import TransformerWrapper, Encoder
+
+encoder = TransformerWrapper(
+    num_tokens = 20000,
+    max_seq_len = 1024,
+    attn_layers = Encoder(
+        dim = 512,
+        depth = 12,
+        heads = 8, 
+        attn_flash = True
+    )
+)
+
+x = torch.randint(0, 256, (1, 1024))
+model(x)
+
+'''
+z = 0
+for i in encoder.parameters():
+    z += i.numel()
+
+print(z)
+'''
